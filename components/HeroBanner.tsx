052e4c24a294de8812cc1e8a7e97347b1c8de607
Syed-rayyan12 <srayyan314@gmail.com>
2026-09-04 19:34:27 +0500

    code updated of slider images

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -8,8 +8,9 @@ const HeroBanner = () => {
             rightImageSrc="/images/pet.png"
             rightImageSlides={[
                 { src: '/images/pet.png', alt: 'Pet' },
-                { src: '/images/about-hero.png', alt: 'Pet care' },
-                { src: '/images/pet.png', alt: 'Pet' },
+                { src: '/images/slide-2.png', alt: 'Pet care' },
+                { src: '/images/slide-3.png', alt: 'Pet' },
+                   { src: '/images/slide-4.png', alt: 'Pet' },
             ]}
             rightImageBottomClassName="bottom-6"
             showSearch
